1fd19af8f6b19ceb74d3fd87e912a55234e836ef
cardize <mustafasamisakarya@gmail.com>
2022-06-28 16:34:51 +0300

    template created

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} />
+      <h1>Heyyy</h1>
+    </div>
+  )
+}
+
+export default Header
