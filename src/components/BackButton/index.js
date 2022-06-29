531d0d37238765f1de55dd02c7d6e4a0b12027d1
cardize <mustafasamisakarya@gmail.com>
2022-06-30 02:05:27 +0300

    scroll template

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const Header = () => {
+const BackButton = () => {
   return (
     <div className="logo-container">
       <NavLink to="/browse">
@@ -11,4 +11,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default BackButton
--- src/pages/PodcastPage/index.js
+++ src/pages/PodcastPage/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import BackButton from '../../components/BackButton'
+import Categories from '../../components/Categories'
 
 const PodcastPage = () => {
   return (
     <div className="App">
       <h1>Podcast Page</h1>
       <BackButton />
+      <Categories />
     </div>
   )
 }
