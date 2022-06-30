640550e964a407a92fedfa05fcd5f06552c7a5c1
cardize <mustafasamisakarya@gmail.com>
2022-06-30 13:13:27 +0300

    removed test component

--- src/pages/PodcastPage/index.js
+++ src/pages/PodcastPage/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import BackButton from '../../components/BackButton'
-import ScrollMenu from '../../components/ScrollMenu'
 
 const PodcastPage = () => {
   return (
     <div className="App">
       <h1>Podcast Page</h1>
       <BackButton />
-      <ScrollMenu />
     </div>
   )
 }
